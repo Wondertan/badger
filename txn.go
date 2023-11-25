5c2a3c48ffd1f40026534d1967fbaaf17da7b981
Wondertan <hlibwondertan@gmail.com>
2023-11-26 00:46:50 +0100

    remove fix for reproducibility

--- txn.go
+++ txn.go
@@ -726,7 +726,6 @@ func (txn *Txn) CommitWith(cb func(error)) {
 		// callback might be acquiring the same locks. Instead run the callback
 		// from another goroutine.
 		go runTxnCallback(&txnCb{user: cb, err: nil})
-		txn.Discard()
 		return
 	}
 
